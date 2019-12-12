439758511c202397a02621a8782500e32a930441
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-12-12 05:05:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   bbq_holidays = []
-  hash.each do |seasons, holidays|
+  holiday_hash.each do |seasons, holidays|
     hash[seasons].each do |holidays, supplies|
       hash[seasons][holidays].each do |supplies|
         if supplies == "BBQ"
